5b879ed40ca51282b0a6526981c119d70729ad1d
lipe-menezes <filipebsmenezes@gmail.com>
2023-09-28 18:45:51 -0300

    Código Final

--- Questão5.c
+++ Questão5.c
@@ -2,15 +2,15 @@
 
 int main() {
     int num1, num2;
-
+  
     scanf("%d", &num1);
-    
-
     scanf("%d", &num2);
+    
     if (num1 == num2) {
-        printf("Os valores lidos sao iguais.\n");
+        printf("Os valores lidos sao iguais\n");
     } else {
         int maior, menor;
+    
         if (num1 > num2) {
             maior = num1;
             menor = num2;
@@ -18,10 +18,13 @@ int main() {
             maior = num2;
             menor = num1;
         }
-        if (maior % menor == 0) {
-            printf(" %d eh maior que %d\n", maior, num1,num2);
+    
+        if (menor == 0) {
+            printf("%d eh maior que 0 %d nao eh multiplo de 0\n", maior, maior);
+        } else if (maior % menor == 0) {
+            printf("%d eh maior que %d  %d eh multiplo de %d\n", maior, menor, maior, menor);
         } else {
-            printf("%d eh maior que %d e nao e multiplo de %d.\n", maior, num1,num2,num2);
+            printf("%d eh maior que %d %d nao eh multiplo de %d\n", maior, menor, maior, menor);
         }
     }
 
